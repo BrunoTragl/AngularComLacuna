951a2a1e520eb841a1a64e9faaae8fde96d60ab5
bruno.tragl <bruno.tragl@TGSSBC-09518D>
2021-11-29 15:40:41 -0300

    instalação do pacote lacuna web pki e a listagem dos certificados instalados na máquina local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import LacunaWebPKI from 'web-pki';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-lacuna-teste';
+  pki: any;
+
+  ngOnInit() {
+    this.pki = new LacunaWebPKI();
+    console.log(this.pki);
+    this.pki.init({
+      ready: this.onReadyPKI() 
+    });
+  }
+
+  onReadyPKI() {
+    console.log(this.pki);
+  }
+
+  teste() {
+    this.pki.listCertificates({
+      success: function (certs) {
+        for (var i = 0; i < certs.length; i++) {
+          var cert = certs[i];
+          var thumbprint = cert.thumbprint;
+          var subjectName = cert.subjectName;
+          var issuerName = cert.issuerName;
+          console.log(certs[i]);
+        }
+      },
+      error: function (errs) {
+        var teste = errs; 
+      }
+    });
+  }
+
+
+  onSuccessListCertificates(certs) {
+    for (var i = 0; i < certs.length; i++) {
+      var cert = certs[i];
+      var thumbprint = cert.thumbprint;
+      var subjectName = cert.subjectName;
+      var issuerName = cert.issuerName;
+      console.log(certs[i]);
+    }
+  }
+
+  onErrorListCertificates(e) {
+    
+  }
 }
